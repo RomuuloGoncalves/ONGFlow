91a8f18dbbf8bf1706679e4a4ddf08665337860e
RomuuloGoncalves <romulogoncalves13@gmail.com>
2025-09-03 23:44:01 -0300

    adicionando funcionalidade de login na API

--- git.sh
+++ git.sh
@@ -10,17 +10,24 @@ fi
 
 COMMIT_MESSAGE="$1"
 
-
 echo "1. Sincronizando com o repositório remoto (git pull)..."
 git pull
 
-echo "2. Adicionando todos os arquivos modificados (git add .)..."
-git add .
+if [[ -n $(git status --porcelain) ]]; then
+  echo -e "\nModificações detectadas. Iniciando processo de commit e push..."
 
-echo "3. Fazendo o commit com a mensagem: \"$COMMIT_MESSAGE\"..."
-git commit -m "$COMMIT_MESSAGE"
+  echo "2. Adicionando todos os arquivos modificados (git add .)..."
+  git add .
 
-echo "4. Enviando as alterações para o repositório remoto (git push)..."
-git push
+  echo "3. Fazendo o commit com a mensagem: \"$COMMIT_MESSAGE\"..."
+  git commit -m "$COMMIT_MESSAGE"
 
-echo -e "\nProcesso concluído com sucesso!"
+  echo "4. Enviando as alterações para o repositório remoto (git push)..."
+  git push
+
+  echo -e "\nAlterações enviadas com sucesso!"
+else
+  echo -e "\nNenhuma alteração detectada. O repositório já está sincronizado."
+fi
+
+echo -e "\nProcesso concluído!"
