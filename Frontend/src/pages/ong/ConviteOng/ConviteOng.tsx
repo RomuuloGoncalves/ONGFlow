fe80f24b17c594d3801ccd1cc6c0dd361e4eea2d
lucas-r-nogueira <ramalhol148@gmail.com>
2025-10-24 16:02:53 -0300

    Parte estrutura Covite ONG

--- Frontend/src/pages/ong/ConviteOng/ConviteOng.tsx
+++ Frontend/src/pages/ong/ConviteOng/ConviteOng.tsx
@@ -1,8 +1,17 @@
 import Header from "@/components/Ong/Header/Header"
+import style from './ConviteOng.module.css'
 
 function ConviteOng() {
   return (
-    <Header />
+    <div className={style.main}>
+      <Header />
+      <div className={style.convite}>
+        <div className={style.convite__title}>
+          <h1>Convites</h1>
+          <p>Envie convites e descubra quem está pronto para se juntar a você. Acompanhe as respostas, veja quem aceitou e forme sua equipe ideal.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
