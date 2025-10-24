7fa6a8e0b3312b2eebb7bf799cece737d4a7f373
lucas-r-nogueira <ramalhol148@gmail.com>
2025-10-24 16:00:34 -0300

    Parte estrutura ListagemVoluntario

--- Frontend/src/pages/ong/ListagemVoluntario/ListagemVoluntario.tsx
+++ Frontend/src/pages/ong/ListagemVoluntario/ListagemVoluntario.tsx
@@ -1,7 +1,33 @@
 import Header from "@/components/Ong/Header/Header";
+import style from "./ListagemVoluntario.module.css";
 
 function ListagemVoluntario() {
-  return <Header />;
+  return (
+    <div className={style.main}>
+      <Header />
+      <div className={style.listagem__voluntario}>
+        <div className={style.listagem__voluntario_title}>
+          <h1>Lista de Voluntários</h1>
+          <div className={style.stats}>
+            <div className={style.voluntario__ativo}>
+              {/* Icon */}
+              <div className={style.text}>
+                <p>Voluntarios Ativos</p>
+                <p></p>
+              </div>
+            </div>
+            <div className={style.voluntario__cadastrado}>
+              {/* Icon */}
+              <div className={style.text}>
+                <p>Voluntarios Cadastrados</p>
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ListagemVoluntario;
