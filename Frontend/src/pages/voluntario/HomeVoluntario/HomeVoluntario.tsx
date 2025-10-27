202f17d3f1600f23ac9d24f6704e84223f700311
lucas-r-nogueira <ramalhol148@gmail.com>
2025-10-27 16:15:37 -0300

    Fix deploy

--- Frontend/src/pages/voluntario/HomeVoluntario/HomeVoluntario.tsx
+++ Frontend/src/pages/voluntario/HomeVoluntario/HomeVoluntario.tsx
@@ -2,7 +2,6 @@ import Header from "@/components/Voluntario/Header/Header";
 import style from "./HomeVoluntario.module.css";
 import { useState, useEffect } from "react";
 import { Pagination } from "@/components/Pagination/Pagination";
-import SelectSimple from "@/components/Voluntario/Select";
 import { Pesquisa } from "@/assets/icons/Pesquisa";
 import { Localizacao } from "@/assets/icons/Localizacao";
 import ModalVoluntario from "@/modals/Voluntarios/VoluntarioHome/modalVoluntarioHome";
